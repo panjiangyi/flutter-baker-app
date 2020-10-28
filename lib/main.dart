d481e6c397e8a1fb300c03a09f8a5626673cfeed
pjy <78237724@qq.com>
2020-10-28 23:41:45 +0800

    2

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,30 @@
 import "package:flutter/material.dart";
 
 void main() {
-  runApp(MyApp("title11!!"));
+  runApp(MyApp("first app"));
 }
 
 class MyApp extends StatelessWidget {
   String title = "default!!";
   MyApp(this.title);
+
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Text(title));
+    final questions = ["1112dd1", "dfdfdfdfs"];
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(title),
+        ),
+        body: Column(
+          children: questions
+              .map((e) => RaisedButton(
+                    child: Text(e.toString()),
+                    onPressed: null,
+                  ))
+              .toList(),
+        ),
+      ),
+    );
   }
 }
