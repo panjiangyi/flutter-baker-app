398fc9e8d90040b864111f26a6fbdb2312b266d5
pjy <78237724@qq.com>
2020-11-12 18:18:55 +0800

    ddd

--- lib/main.dart
+++ lib/main.dart
@@ -77,26 +77,29 @@ class _TapboxCState extends State<TapboxC> {
   Widget build(BuildContext context) {
     // This example adds a green border on tap down.
     // On tap up, the square changes to the opposite state.
-    return GestureDetector(
-      onTapDown: _handleTapDown, // Handle the tap events in the order that
-      onTapUp: _handleTapUp, // they occur: down, up, tap, cancel
-      onTap: _handleTap,
-      onTapCancel: _handleTapCancel,
-      child: Container(
-        child: Center(
-          child: Text(widget.active ? 'Active' : 'Inactive',
-              style: TextStyle(fontSize: 32.0, color: Colors.white)),
-        ),
-        width: 200.0,
-        height: 200.0,
-        decoration: BoxDecoration(
-          color: widget.active ? Colors.lightGreen[700] : Colors.grey[600],
-          border: _highlight
-              ? Border.all(
-                  color: Colors.teal[700],
-                  width: 10.0,
-                )
-              : null,
+    return Directionality(
+      textDirection: TextDirection.ltr,
+      child: GestureDetector(
+        onTapDown: _handleTapDown, // Handle the tap events in the order that
+        onTapUp: _handleTapUp, // they occur: down, up, tap, cancel
+        onTap: _handleTap,
+        onTapCancel: _handleTapCancel,
+        child: Container(
+          child: Center(
+            child: Text(widget.active ? 'Active' : 'Inactive',
+                style: TextStyle(fontSize: 32.0, color: Colors.white)),
+          ),
+          width: 200.0,
+          height: 200.0,
+          decoration: BoxDecoration(
+            color: widget.active ? Colors.lightGreen[700] : Colors.grey[600],
+            border: _highlight
+                ? Border.all(
+                    color: Colors.teal[700],
+                    width: 10.0,
+                  )
+                : null,
+          ),
         ),
       ),
     );
