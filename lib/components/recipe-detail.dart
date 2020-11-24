a4f13d21fa0d1274d2e64e1512bc1a96e3296ca9
pjy <78237724@qq.com>
2020-11-24 18:02:45 +0800

    update

--- lib/components/recipe-detail.dart
+++ lib/components/recipe-detail.dart
@@ -1,4 +1,5 @@
 import "package:flutter/material.dart";
+import 'package:flutter/services.dart';
 
 class RecipeDetail extends StatelessWidget {
   final String title;
@@ -10,14 +11,25 @@ class RecipeDetail extends StatelessWidget {
       appBar: AppBar(
         title: Text(title),
       ),
-      body: Center(
-        child: FlatButton(
-          child: Text(title),
-          onPressed: () {
-            Navigator.pop(context);
-          },
-        ),
-      ),
+      body: Column(
+          children: ["蛋糕", "面包"].map((i) {
+        return Row(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(right: 20),
+              child: Text("$i："),
+            ),
+            Expanded(
+                child: new TextField(
+              decoration: new InputDecoration(labelText: "输入百分比"),
+              keyboardType: TextInputType.number,
+              inputFormatters: <TextInputFormatter>[
+                FilteringTextInputFormatter.digitsOnly
+              ], // Only numbers can be entered
+            )),
+          ],
+        );
+      }).toList()),
     );
   }
 }
